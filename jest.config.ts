51871cb5a2eeff0f830d68e2e3c55f22059d1d53
Matthew Springer <mtrspringer@users.noreply.github.com>
2020-11-03 16:06:17 -0500

    initial commit - tests passing

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+// jest.config.ts
+import type { Config } from '@jest/types';
+
+// Sync object
+const config: Config.InitialOptions = {
+  moduleFileExtensions: [
+    "js",
+    "json",
+    "ts"
+  ],
+  rootDir: "src",
+  testRegex: ".*\\.spec\\.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest"
+  },
+  coverageDirectory: "./coverage",
+  testEnvironment: "node"
+};
+
+export default config;
